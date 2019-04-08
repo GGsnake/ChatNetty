113b501f14f7d7b2c70adde5587d82aeebdcf7b7
GGsnake <ljp462693266@outlook.com>
2019-04-08 19:58:09 +0800

    集成消息队列

--- src/main/java/com/webscoket/webscoket/WebscoketApplication.java
+++ src/main/java/com/webscoket/webscoket/WebscoketApplication.java
@@ -1,9 +1,11 @@
 package com.webscoket.webscoket;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class WebscoketApplication {
 
     public static void main(String[] args) {
