e4be60f119da0d6cbdaf90261d79f9f5532f7784
GGsnake <ljp462693266@outlook.com>
2019-05-28 21:14:25 +0800

    完善结构层

--- src/main/java/com/webscoket/webscoket/service/MyWebSocket.java
+++ src/main/java/com/webscoket/webscoket/service/MyWebSocket.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 
-@ServerEndpoint("/websocket/{sid}")
+@ServerEndpoint("/websocket")
 @Component
 @Log
 public class MyWebSocket {
